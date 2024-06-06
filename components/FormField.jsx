019bc3a62c0f8ad7fc84360b22dd1134d9d56e25
Fede <federicojose2000@gmail.com>
2024-06-06 23:39:03 +0200

    fixed icons img

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 
-import { icons } from "../constants/icons";
+import { icons } from "../constants";
 
 const FormField = ({
   title,
